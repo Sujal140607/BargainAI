056499b4deb10d706f1f747c15fa30019036f209
Sujal140607 <skp140607@gmail.com>
2026-07-03 11:58:41 +0530

    feat: connect MongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 import app from './src/app.js';
+import connectDB from './src/config/db.js';
+import dns from 'node:dns/promises'
+dns.setServers(['8.8.8.8', '1.1.1.1'])
 import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
